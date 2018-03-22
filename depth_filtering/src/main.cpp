b1468dcb9029022219a168af559fc8cdca50b389
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-03-22 19:13:22 +0900

    detect object
    blob detection

--- depth_filtering/src/main.cpp
+++ depth_filtering/src/main.cpp
@@ -7,6 +7,23 @@
 #include <vector>
 
 using namespace cv;
+using namespace std;
+
+RNG rng(12345);
+int thresh = 100;
+int max_thresh = 255;
+
+vector<Point> contoursConvexHull(vector<vector<Point> > contours)
+{
+	vector<Point> result;
+	vector<Point> pts;
+	for (size_t i = 0; i< contours.size(); i++)
+		for (size_t j = 0; j< contours[i].size(); j++)
+			pts.push_back(contours[i][j]);
+	convexHull(pts, result);
+	return result;
+}
+
 void main()
 {
 	try {
@@ -28,6 +45,23 @@ void main()
 		streams.push_back(&depthStream);
 		cv::Mat depthImage;
 
+		int erosion_size = 6;
+		Mat element = getStructuringElement(cv::MORPH_DILATE,
+			cv::Size(2 * erosion_size + 1, 2 * erosion_size + 1),
+			cv::Point(erosion_size, erosion_size));
+
+		// Set up the detector with default parameters.
+		SimpleBlobDetector::Params params;
+
+		params.filterByInertia = false;
+		params.filterByConvexity = false;
+		params.filterByArea = true;
+		params.minArea = 1000;
+		params.maxArea = 10000;
+		params.filterByColor = true;
+		params.blobColor = 255;
+		Ptr<SimpleBlobDetector> d = SimpleBlobDetector::create(params);
+
 		while (1) {
 			int changedIndex;
 
@@ -40,22 +74,24 @@ void main()
 						depthStream.getVideoMode().getResolutionX(),
 						CV_16U, (char*)depthFrame.getData());
 
-					// 0-10000mmまでのデータを0-255にする
-					depthImage.setTo(0, depthImage > 2500);
-					depthImage.convertTo(depthImage, CV_8U, 255.0 / 2500);
+					// 0-2000mmまでのデータを0-255にする
+					depthImage.setTo(0, depthImage > 1500);
+					depthImage.convertTo(depthImage, CV_8U, 255.0 / 1500);
+					
+					Mat threshImage;
+					threshold(depthImage, threshImage, 30, 255, THRESH_BINARY);
+					dilate(threshImage, threshImage, element);
 
-					// 中心点の距離を表示する
-					auto videoMode = depthStream.getVideoMode();
-					int centerX = videoMode.getResolutionX() / 2;
-					int centerY = videoMode.getResolutionY() / 2;
-					int centerIndex = (centerY * videoMode.getResolutionX()) + centerX;
-					short* depth = (short*)depthFrame.getData();
+					// Detect blobs.
+					std::vector<KeyPoint> keypoints;
+					d->detect(threshImage, keypoints);
 
-					std::stringstream ss;
-					ss << "Center Point :" << depth[centerIndex];
-					cv::putText(depthImage, ss.str(), cv::Point(0, 50),
-						cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 0, 0), 1);
-					cv::imshow("Depth Camera", depthImage);
+					// Draw detected blobs as red circles.
+					// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures the size of the circle corresponds to the size of blob
+					Mat im_with_keypoints;
+					drawKeypoints(threshImage, keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+					imshow("keypoints", im_with_keypoints);
 				}
 			}
 
@@ -69,3 +105,4 @@ void main()
 		std::cout << openni::OpenNI::getExtendedError() << std::endl;
 	}
 }
+
