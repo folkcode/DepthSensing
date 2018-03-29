a413d291a47ac077330ea47583f55a3cf4a839ad
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2018-03-29 19:26:47 +0900

    OSC
    Send blob keypoints through OSC to port 5001

--- depth_filtering/src/main.cpp
+++ depth_filtering/src/main.cpp
@@ -6,12 +6,21 @@
 
 #include <vector>
 
+// oscpack
+#include <ip/UdpSocket.h>
+#include <osc/OscOutboundPacketStream.h>
+
 using namespace cv;
 using namespace std;
 
 RNG rng(12345);
 int thresh = 100;
 int max_thresh = 255;
+
+#define OUTPUT_BUFFER_SIZE 1024
+
+const char* ip_address = "127.0.0.1";
+unsigned int port = 5001;
 
 vector<Point> contoursConvexHull(vector<vector<Point> > contours)
 {
@@ -26,6 +35,8 @@ vector<Point> contoursConvexHull(vector<vector<Point> > contours)
 
 void main()
 {
+	stringstream data_string;
+	data_string.str(std::string());
 	try {
 		openni::OpenNI::initialize();
 		openni::Device device;
@@ -62,8 +73,14 @@ void main()
 		params.blobColor = 255;
 		Ptr<SimpleBlobDetector> d = SimpleBlobDetector::create(params);
 
+		UdpTransmitSocket transmitSocket(IpEndpointName(ip_address, port));
+
 		while (1) {
+
 			int changedIndex;
+
+			// Clear the data string variable every frame;
+			data_string.str(std::string());
 
 			openni::OpenNI::waitForAnyStream(&streams[0], streams.size(), &changedIndex);
 			if (changedIndex == 0) {
@@ -91,6 +108,22 @@ void main()
 					Mat im_with_keypoints;
 					drawKeypoints(threshImage, keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 
+					if (keypoints.size() >= 1) {
+						for (int i = 0; i < keypoints.size(); i++) {
+							Point2f point = keypoints[i].pt;
+							data_string << to_string(point.x) << "," << to_string(point.y);
+						}
+					}
+
+					char buffer[OUTPUT_BUFFER_SIZE];
+					osc::OutboundPacketStream ps(buffer, OUTPUT_BUFFER_SIZE);
+					string send_string = data_string.str();
+					send_string += "end";
+					
+					char *toSendText = new char[send_string.size() + 1];
+					std::strcpy(toSendText, send_string.c_str());
+					transmitSocket.Send(toSendText, strlen(toSendText) + 1);
+					cout << send_string.size() << endl;
 					imshow("keypoints", im_with_keypoints);
 				}
 			}
@@ -100,6 +133,11 @@ void main()
 				break;
 			}
 		}
+		// TODO : close device and other cleanup
+		depthStream.stop();
+		depthStream.destroy();
+		device.close();
+		openni::OpenNI::shutdown();
 	}
 	catch (std::exception&) {
 		std::cout << openni::OpenNI::getExtendedError() << std::endl;
